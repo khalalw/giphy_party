b1ed0394ce02c6aefaeee568b05fb4e7eda2986b
Khalal Walker <khalal.walker@gmail.com>
2019-01-03 18:24:07 -0800

    complete giphy functionaliy and add css grid

---
+++ main.js
@@ -0,0 +1,38 @@
+$(function() {
+  let form = document.querySelector('form');
+  let clearBtn = document.querySelector('#clear');
+  let apiKey = '33sv4ePMBpRH7dhg134wMIwoZqtgvUeT';
+  let gifCont = document.querySelector('#gif-cont');
+
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    let query = document.querySelector('#query').value;
+
+    $.get('http://api.giphy.com/v1/gifs/search', {
+      q: query,
+      api_key: apiKey,
+    }).then(function(response) {
+      let data = response.data;
+      console.log(response);
+
+      if (data.length) {
+        let randomId = Math.floor(Math.random() * data.length);
+        let gif = document.createElement('img');
+        let gifDiv = document.createElement('div');
+        gifDiv.addClass = 'gif-div';
+
+        gif.src = data[randomId].images.original.url;
+        gif.style.height = '200px';
+        gif.style.width = '300px';
+
+        gifDiv.appendChild(gif);
+        gifCont.appendChild(gifDiv);
+      }
+    });
+  });
+
+  clearBtn.addEventListener('click', function(e) {
+    gifCont.innerHTML = '';
+    document.querySelector('#query').value = '';
+  });
+});
