9dcefcf4a7a30afa5fe18fa40c721431c7deffe0
Khalal Walker <khalal.walker@gmail.com>
2019-01-03 18:47:16 -0800

    update styling

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-$(function() {
+$(function () {
+  $.backstretch('https://images.unsplash.com/photo-1446716336919-df838e44ce7b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1345&q=80');
+
   let form = document.querySelector('form');
   let clearBtn = document.querySelector('#clear');
   let apiKey = '33sv4ePMBpRH7dhg134wMIwoZqtgvUeT';
   let gifCont = document.querySelector('#gif-cont');
 
-  form.addEventListener('submit', function(e) {
+  form.addEventListener('submit', function (e) {
     e.preventDefault();
     let query = document.querySelector('#query').value;
 
     $.get('http://api.giphy.com/v1/gifs/search', {
       q: query,
       api_key: apiKey,
-    }).then(function(response) {
+    }).then(function (response) {
       let data = response.data;
       console.log(response);
 
@@ -31,7 +33,7 @@ $(function() {
     });
   });
 
-  clearBtn.addEventListener('click', function(e) {
+  clearBtn.addEventListener('click', function (e) {
     gifCont.innerHTML = '';
     document.querySelector('#query').value = '';
   });
